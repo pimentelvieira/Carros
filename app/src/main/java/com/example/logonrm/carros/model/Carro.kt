aa5d5908c118243212beed8f90f9b464995b8a30
logonrm <logonrm@fiap.com.br>
2018-03-15 23:02:00 -0300

    Alteracoes

--- app/src/main/java/com/example/logonrm/carros/model/Carro.kt
+++ app/src/main/java/com/example/logonrm/carros/model/Carro.kt
@@ -1,6 +1,6 @@
 package com.example.logonrm.carros.model
 
-data class Carro(var id: Int?,
+data class Carro(var id: String?,
                  var marca: String?,
                  var modelo: String,
                  var ano: Int,
